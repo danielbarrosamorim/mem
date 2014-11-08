bfbf19ce0c73440ccddcb0e620e037da2869d94b
danielbarrosamorim <daniel.amorim@gmail.com>
2014-11-08 17:15:51 -0200

    Initial check-in of module PlatformVisuals

---
+++ PlatformVisuals/PlatformVisuals.WinPhone/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+using Xamarin.Forms;
+
+
+namespace PlatformVisuals.WinPhone
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            SupportedOrientations = SupportedPageOrientation.PortraitOrLandscape;
+
+            Forms.Init();
+            Content = PlatformVisuals.App.GetMainPage().ConvertPageToUIElement(this);
+        }
+    }
+}
---
+++ PlatformVisuals/PlatformVisuals/PlatformVisualsPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlatformVisuals
+{
+    public partial class PlatformVisualsPage
+    {
+        public PlatformVisualsPage()
+        {
+
+            InitializeComponent();
+			//butao.
+        }
+    }
+}
